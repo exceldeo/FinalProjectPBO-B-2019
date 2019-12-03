5a548252dcc3ea4455f70b6c5ad9aa210e4ca1fe
Clever Dicki <48941105+cleverdicki@users.noreply.github.com>
2019-12-03 10:11:27 +0700

    Membuat class baru untuk menghitung waktu
    Ini akan digunakan untuk menghitung waktu di mode time attack

---
+++ src/id/ac/its/KED/snakegame/TimeCounter.java
@@ -0,0 +1,50 @@
+package id.ac.its.KED.snakegame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TimeCounter implements ActionListener {
+
+    private Timer clock;
+    private int hours = 0;
+    private int min = 0;
+    private int sec = 0;
+
+    public TimeCounter(int hours, int min, int sec) {
+        this.hours = hours;
+        this.min = min;
+        this.sec = sec;
+
+        clock = new Timer(15, this);
+        clock.start();
+    }
+
+    public String reslt()
+    {
+        return hours + ":" + min + ":" + sec;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        {
+            if(e.getSource() == clock) {
+                sec++;
+            }
+            if(sec == 60)
+            {
+                min++;
+                sec = 0;
+            }
+            if(min == 60)
+            {
+                hours++;
+                min = 0;
+            }
+        }
+    }
+
+    public void actionPerformed() {
+
+    }
+}

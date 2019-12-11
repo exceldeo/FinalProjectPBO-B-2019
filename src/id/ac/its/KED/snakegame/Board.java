472c090d31e8c68fd2b00b57461017e03671b608
Clever Dicki <48941105+cleverdicki@users.noreply.github.com>
2019-12-11 13:46:45 +0700

    Mengganti UI game
    Mengganti warna obstacle pada kedua mode, mengganti warna tulisan "SCORE", dan menambahkan gambar tulisan "GAME OVER!"

--- src/id/ac/its/KED/snakegame/Board.java
+++ src/id/ac/its/KED/snakegame/Board.java
@@ -79,6 +79,8 @@ public class Board extends JPanel implements ActionListener {
     private Image obstacle;
     private Image header;
     private Image backGroundGame;
+    private Image obstacle2;
+    private Image gameOverText;
 
     private TimeCounter timeCounter;
 
@@ -126,7 +128,7 @@ public class Board extends JPanel implements ActionListener {
         final ImageIcon iih = new ImageIcon("src/resources/images/righthead.png");
         head = iih.getImage();
 
-        final ImageIcon iio = new ImageIcon("src/resources/images/obstacle.png");
+        final ImageIcon iio = new ImageIcon("src/resources/images/obstacle1.png");
         obstacle = iio.getImage();
         
         final ImageIcon iib = new ImageIcon("src/resources/images/backgroundgame.png");
@@ -134,6 +136,12 @@ public class Board extends JPanel implements ActionListener {
 
         final ImageIcon iiheader = new ImageIcon("src/resources/images/header.png");
         header = iiheader.getImage();
+
+        final ImageIcon iiob = new ImageIcon("src/resources/images/obstacle2.png");
+        obstacle2 = iiob.getImage();
+
+        final ImageIcon iigo = new ImageIcon("src/resources/images/gameover.png");
+        gameOverText = iigo.getImage();
     }
 
     private void backgroundMusic() {
@@ -302,7 +310,7 @@ public class Board extends JPanel implements ActionListener {
                     // --> Bagian untuk syntax obstacles
                     for (int a = 0; a < 320; a++) {
                     	
-                        g.drawImage(obstacle, obsXP[a], obsYP[a], this);
+                        g.drawImage(obstacle2, obsXP[a], obsYP[a], this);
                     }
                     
                     for (int z = 0; z < dots; z++) {
@@ -334,7 +342,7 @@ public class Board extends JPanel implements ActionListener {
         String s;
 
         g.setFont(smallFont);
-        g.setColor(new Color(96, 128, 255));
+        g.setColor(new Color(0, 0, 0));
         s = "Score: " + score;
         g.drawString(s, SCREEN_SIZE / 7, SCREEN_SIZE / 10);
     }
@@ -382,11 +390,9 @@ public class Board extends JPanel implements ActionListener {
     	
         if (typeG == 1) {
 
-            final String msg = "Game Over !";
+            g.drawImage(gameOverText, 170, 170, this);
             final String msg1 = "Score : " + score;
             
-            g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2 - 40);
-              
             if (score < 10) {
 
                 g.drawString(msg1, (B_WIDTH - metr.stringWidth(msg1)) / 2, B_HEIGHT / 2);
@@ -406,10 +412,9 @@ public class Board extends JPanel implements ActionListener {
 
         else if (typeG == 2) {
 
-            final String msg = "Game Over !";
             final String msg1 = "Score : " + score + "    Time: " + timeCounter.result();
 
-            g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2);
+            g.drawImage(gameOverText, 170, 170, this);
             
             if (score < 10) {
 
